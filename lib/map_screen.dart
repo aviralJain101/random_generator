1a86d5566062c41c98d259b413589883ad7ff88b
aviralJain101 <aviraljain.ece18@itbhu.ac.in>
2024-04-11 16:50:07 +0530

    removed dialogue box in showValue button

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -14,25 +14,30 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   String _randomKey = '';
+  bool _isShowValue = false;
 
   void _generateRandomKey() {
     if (widget.mapData.data.isEmpty) {
       return; // Handle case of empty map gracefully
     }
     setState(() {
+      _isShowValue = false;
       _randomKey = widget.mapData.data.keys.toList()[Random().nextInt(widget.mapData.data.length)];
     });
   }
 
   void _showValue() {
     if (_randomKey.isEmpty) return;
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Value'),
-        content: Text(widget.mapData.data[_randomKey]!),
-      ),
-    );
+    setState(() {
+      _isShowValue = true;
+    });
+    // showDialog(
+    //   context: context,
+    //   builder: (context) => AlertDialog(
+    //     title: const Text('Value', style: TextStyle(fontSize: 20),),
+    //     content: Text(widget.mapData.data[_randomKey]!, style: const TextStyle(fontSize: 40),),
+    //   ),
+    // );
   }
 
   @override
@@ -46,20 +51,28 @@ class _MapScreenState extends State<MapScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              _randomKey.isEmpty ? 'No key generated yet' : 'Random Key: $_randomKey',
-              style: const TextStyle(fontSize: 20),
+              _randomKey.isEmpty ? 'No key generated yet' : _randomKey,
+              style: const TextStyle(fontSize: 40),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 40),
             ElevatedButton(
               onPressed: _generateRandomKey,
               child: const Text('Generate Random Key'),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 40),
             if (_randomKey.isNotEmpty) // Only show button if a key is generated
               ElevatedButton(
                 onPressed: _showValue,
                 child: const Text('Show Value'),
               ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 50,
+              child: Text(
+                _isShowValue ? widget.mapData.data[_randomKey]! : '',
+                style: const TextStyle(fontSize: 40),
+              ),
+            ),
           ],
         ),
       ),
